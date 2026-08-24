ca138721b37e1efeed65594b7603a3f861a92be9
nafiisahzalfa4 <zalfaa.nafiisah28@smk.belajar.id>
2026-08-24 14:44:13 +0700

    route new

--- routes/web.php
+++ routes/web.php
@@ -11,71 +11,37 @@ Route::get('/', function () {
 });
 
 // ==================== ROUTE KATEGORI KERUSAKAN ====================
-Route::prefix('KategoriKerusakan')->name('KategoriKerusakan.')->group(function () {
-    Route::get('/', [KategoriKerusakanController::class, 'index'])->name('index');
-    Route::get('/create', [KategoriKerusakanController::class, 'create'])->name('create');
-    Route::post('/', [KategoriKerusakanController::class, 'store'])->name('store');
-    Route::get('/{id}', [KategoriKerusakanController::class, 'show'])->name('show');
-    Route::get('/{id}/edit', [KategoriKerusakanController::class, 'edit'])->name('edit');
-    Route::put('/{id}', [KategoriKerusakanController::class, 'update'])->name('update');
-    Route::delete('/{id}', [KategoriKerusakanController::class, 'destroy'])->name('destroy');
-});
+Route::get('/KategoriKerusakan', [KategoriKerusakanController::class, 'index'])->name('KategoriKerusakan.index');
+Route::get('/KategoriKerusakan/create', [KategoriKerusakanController::class, 'create'])->name('KategoriKerusakan.create');
+Route::post('/KategoriKerusakan', [KategoriKerusakanController::class, 'store'])->name('KategoriKerusakan.store');
+Route::get('/KategoriKerusakan/{id}', [KategoriKerusakanController::class, 'show'])->name('KategoriKerusakan.show');
+Route::get('/KategoriKerusakan/{id}/edit', [KategoriKerusakanController::class, 'edit'])->name('KategoriKerusakan.edit');
+Route::put('/KategoriKerusakan/{id}', [KategoriKerusakanController::class, 'update'])->name('KategoriKerusakan.update');
+Route::delete('/KategoriKerusakan/{id}', [KategoriKerusakanController::class, 'destroy'])->name('KategoriKerusakan.destroy');
 
 // ==================== ROUTE KONTEN EDUKASI ====================
-Route::prefix('KontenEdukasi')->name('KontenEdukasi.')->group(function () {
-    Route::get('/', [KontenEdukasiController::class, 'index'])->name('index');
-    Route::get('/create', [KontenEdukasiController::class, 'create'])->name('create');
-    Route::post('/', [KontenEdukasiController::class, 'store'])->name('store');
-    Route::get('/{id}', [KontenEdukasiController::class, 'show'])->name('show');
-    Route::get('/{id}/edit', [KontenEdukasiController::class, 'edit'])->name('edit');
-    Route::put('/{id}', [KontenEdukasiController::class, 'update'])->name('update');
-    Route::delete('/{id}', [KontenEdukasiController::class, 'destroy'])->name('destroy');
-    
-    // Additional routes
-    Route::get('/status/{status}', [KontenEdukasiController::class, 'getByStatus'])->name('status');
-    Route::get('/kategori/{kategori}', [KontenEdukasiController::class, 'getByKategori'])->name('kategori');
-    Route::get('/penulis/{penulisId}', [KontenEdukasiController::class, 'getByPenulis'])->name('penulis');
-    Route::put('/{id}/publish', [KontenEdukasiController::class, 'publish'])->name('publish');
-    Route::put('/{id}/nonaktif', [KontenEdukasiController::class, 'nonaktif'])->name('nonaktif');
-});
+Route::get('/KontenEdukasi', [KontenEdukasiController::class, 'index'])->name('KontenEdukasi.index');
+Route::get('/KontenEdukasi/create', [KontenEdukasiController::class, 'create'])->name('KontenEdukasi.create');
+Route::post('/KontenEdukasi', [KontenEdukasiController::class, 'store'])->name('KontenEdukasi.store');
+Route::get('/KontenEdukasi/{id}', [KontenEdukasiController::class, 'show'])->name('KontenEdukasi.show');
+Route::get('/KontenEdukasi/{id}/edit', [KontenEdukasiController::class, 'edit'])->name('KontenEdukasi.edit');
+Route::put('/KontenEdukasi/{id}', [KontenEdukasiController::class, 'update'])->name('KontenEdukasi.update');
+Route::delete('/KontenEdukasi/{id}', [KontenEdukasiController::class, 'destroy'])->name('KontenEdukasi.destroy');
 
 // ==================== ROUTE LAPORAN ====================
-Route::prefix('Laporan')->name('Laporan.')->group(function () {
-    Route::get('/', [LaporanController::class, 'index'])->name('index');
-    Route::get('/create', [LaporanController::class, 'create'])->name('create');
-    Route::post('/', [LaporanController::class, 'store'])->name('store');
-    Route::get('/{id}', [LaporanController::class, 'show'])->name('show');
-    Route::get('/{id}/edit', [LaporanController::class, 'edit'])->name('edit');
-    Route::put('/{id}', [LaporanController::class, 'update'])->name('update');
-    Route::delete('/{id}', [LaporanController::class, 'destroy'])->name('destroy');
-    
-    // Additional routes
-    Route::put('/{id}/verify', [LaporanController::class, 'verify'])->name('verify');
-    Route::get('/status/{status}', [LaporanController::class, 'getByStatus'])->name('status');
-    Route::get('/user/{userId}', [LaporanController::class, 'getByUser'])->name('user');
-    Route::get('/prioritas/{tingkat}', [LaporanController::class, 'getByPrioritas'])->name('prioritas');
-    Route::get('/instansi/{instansiId}', [LaporanController::class, 'getByInstansi'])->name('instansi');
-    Route::get('/kategori/{kategoriId}', [LaporanController::class, 'getByKategori'])->name('kategori');
-    Route::put('/{id}/proses', [LaporanController::class, 'proses'])->name('proses');
-    Route::put('/{id}/selesai', [LaporanController::class, 'selesai'])->name('selesai');
-    Route::put('/{id}/tolak', [LaporanController::class, 'tolak'])->name('tolak');
-});
+Route::get('/Laporan', [LaporanController::class, 'index'])->name('Laporan.index');
+Route::get('/Laporan/create', [LaporanController::class, 'create'])->name('Laporan.create');
+Route::post('/Laporan', [LaporanController::class, 'store'])->name('Laporan.store');
+Route::get('/Laporan/{id}', [LaporanController::class, 'show'])->name('Laporan.show');
+Route::get('/Laporan/{id}/edit', [LaporanController::class, 'edit'])->name('Laporan.edit');
+Route::put('/Laporan/{id}', [LaporanController::class, 'update'])->name('Laporan.update');
+Route::delete('/Laporan/{id}', [LaporanController::class, 'destroy'])->name('Laporan.destroy');
 
 // ==================== ROUTE NOTIFIKASI ====================
-Route::prefix('Notifikasi')->name('Notifikasi.')->group(function () {
-    Route::get('/', [NotifikasiController::class, 'index'])->name('index');
-    Route::get('/create', [NotifikasiController::class, 'create'])->name('create');
-    Route::post('/', [NotifikasiController::class, 'store'])->name('store');
-    Route::get('/{id}', [NotifikasiController::class, 'show'])->name('show');
-    Route::get('/{id}/edit', [NotifikasiController::class, 'edit'])->name('edit');
-    Route::put('/{id}', [NotifikasiController::class, 'update'])->name('update');
-    Route::delete('/{id}', [NotifikasiController::class, 'destroy'])->name('destroy');
-    
-    // Additional routes
-    Route::get('/unread', [NotifikasiController::class, 'getUnread'])->name('unread');
-    Route::put('/{id}/read', [NotifikasiController::class, 'markAsRead'])->name('read');
-    Route::put('/read-all', [NotifikasiController::class, 'markAllAsRead'])->name('read-all');
-    Route::get('/type/{tipe}', [NotifikasiController::class, 'getByType'])->name('type');
-    Route::get('/count', [NotifikasiController::class, 'getCount'])->name('count');
-    Route::get('/latest', [NotifikasiController::class, 'getLatest'])->name('latest');
-});
+Route::get('/Notifikasi', [NotifikasiController::class, 'index'])->name('Notifikasi.index');
+Route::get('/Notifikasi/create', [NotifikasiController::class, 'create'])->name('Notifikasi.create');
+Route::post('/Notifikasi', [NotifikasiController::class, 'store'])->name('Notifikasi.store');
+Route::get('/Notifikasi/{id}', [NotifikasiController::class, 'show'])->name('Notifikasi.show');
+Route::get('/Notifikasi/{id}/edit', [NotifikasiController::class, 'edit'])->name('Notifikasi.edit');
+Route::put('/Notifikasi/{id}', [NotifikasiController::class, 'update'])->name('Notifikasi.update');
+Route::delete('/Notifikasi/{id}', [NotifikasiController::class, 'destroy'])->name('Notifikasi.destroy');
